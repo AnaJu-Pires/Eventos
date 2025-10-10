da75ad3039a0a675a7f908c9c403dc3e9f7fc8ad
AnaJu-Pires <anaju001@hotmail.com>
2025-10-10 18:31:04 -0300

    Organização Inicial Projeto

---
+++ src/main/java/br/ifsp/events/exception/BusinessRuleException.java
@@ -0,0 +1,7 @@
+package br.ifsp.events.exception;
+
+public class BusinessRuleException extends RuntimeException {
+    public BusinessRuleException(String message) {
+        super(message);
+    }
+}
