428717bba79b14d946be8365ced7471748da8ceb
Rodrigoqifsp <freitas.rodrigo@aluno.ifsp.edu.br>
2025-10-20 03:08:23 -0300

    Tentativa B08 - Edição Parcial de Evento (PATCH)

---
+++ src/main/java/br/ifsp/events/service/EventService.java
@@ -0,0 +1,15 @@
+package br.ifsp.events.service;
+
+import br.ifsp.events.dto.event.EventPatchDTO;
+import br.ifsp.events.dto.event.EventRequestDTO;
+import br.ifsp.events.dto.event.EventResponseDTO;
+
+public interface EventService {
+    EventResponseDTO create(EventRequestDTO eventRequestDTO);
+
+    EventResponseDTO update(Long id, EventRequestDTO eventRequestDTO);
+
+    EventResponseDTO patch(Long id, EventPatchDTO eventPatchDTO);
+
+    void delete(Long id);
+}
